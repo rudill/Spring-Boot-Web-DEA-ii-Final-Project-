4a4ff84f32bf6813aee0c64ea8d7405b7ee9ce01
NadeemalTM <nadeemal@example.com>
2026-01-29 15:42:19 +0530

    Add ReservationManagementService module with initial setup, configuration, and basic structure

---
+++ backend/reservationManagementService/src/main/java/com/nsbm/group03/reservationManagementService/ReservationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.reservationManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/reservationManagementService/src/test/java/com/nsbm/group03/reservationManagementService/ReservationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.reservationManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
