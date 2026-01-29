9eee4e7e4f8b38c80fac8b5dc908cbff2e06e373
NadeemalTM <nadeemal@example.com>
2026-01-29 15:37:05 +0530

    Add kitchenManagementService module with initial setup, configuration, and basic structure

---
+++ backend/kitchenManagementService/src/main/java/com/nsbm/group03/kitchenManagementService/KitchenManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.kitchenManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/kitchenManagementService/src/test/java/com/nsbm/group03/kitchenManagementService/KitchenManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.kitchenManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
