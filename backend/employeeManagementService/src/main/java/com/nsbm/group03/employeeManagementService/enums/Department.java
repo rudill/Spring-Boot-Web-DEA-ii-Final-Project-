2640d938257fe2833adbee081f775c7739f50bc8
NadeemalTM <gayanthanadeemal@gmail.com>
2026-02-18 12:54:48 +0530

    feat: Add Department and EmployeeStatus enums for standardization

---
+++ backend/employeeManagementService/src/main/java/com/nsbm/group03/employeeManagementService/enums/Department.java
@@ -0,0 +1,15 @@
+package com.nsbm.group03.employeeManagementService.enums;
+
+public enum Department {
+    FRONT_DESK,
+    HOUSEKEEPING,
+    KITCHEN,
+    RESTAURANT,
+    MAINTENANCE,
+    MANAGEMENT,
+    SECURITY,
+    EVENTS,
+    HR,
+    FINANCE,
+    IT
+}
---
+++ backend/employeeManagementService/src/main/java/com/nsbm/group03/employeeManagementService/enums/EmployeeStatus.java
@@ -0,0 +1,9 @@
+package com.nsbm.group03.employeeManagementService.enums;
+
+public enum EmployeeStatus {
+    ACTIVE,
+    INACTIVE,
+    ON_LEAVE,
+    TERMINATED,
+    PROBATION
+}
