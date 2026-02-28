d5cd517c94266b0d2c02c6f47418d3728d1871e6
NadeemalTM <nadeemal@example.com>
2026-02-28 11:16:36 +0530

    Remove OrderDetails, OrderList, Statistics, TableForm, and TableList components along with their associated styles and API service files

--- backend/restaurantManagementService/src/main/java/com/nsbm/group03/restaurantManagementService/enums/TableStatus.java
+++
@@ -1,8 +0,0 @@
-package com.nsbm.group03.restaurantManagementService.enums;
-
-public enum TableStatus {
-    AVAILABLE,
-    OCCUPIED,
-    RESERVED,
-    OUT_OF_SERVICE
-}
--- backend/restaurantManagementService/src/main/java/com/nsbm/group03/restaurantManagementService/exception/DuplicateResourceException.java
+++
@@ -1,8 +0,0 @@
-package com.nsbm.group03.restaurantManagementService.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
