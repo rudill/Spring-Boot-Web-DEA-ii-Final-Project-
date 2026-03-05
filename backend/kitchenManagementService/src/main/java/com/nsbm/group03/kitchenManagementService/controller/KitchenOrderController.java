f5b0833209c4916be0c59f056b9e1b2cca351127
Milanka08 <milankamuthuni5@gmail.com>
2026-03-05 09:12:25 +0530

    refactor(kitchen): update KitchenMenuItem and KitchenOrder controllers

--- backend/kitchenManagementService/src/main/java/com/nsbm/group03/kitchenManagementService/controller/KitchenOrderController.java
+++ backend/kitchenManagementService/src/main/java/com/nsbm/group03/kitchenManagementService/controller/KitchenOrderController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,7 +25,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/kitchen/orders")
-@CrossOrigin(origins = "*")
+
 @Validated
 public class KitchenOrderController {
 
